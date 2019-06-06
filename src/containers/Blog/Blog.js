f72f74bcf46f8983a518ccb4b74bc315813c286f
David M <mullaned@gmail.com>
2019-06-06 08:42:26 +0100

    transform data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,16 +4,41 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import Axios from 'axios'
+import axios from 'axios'
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Dave'
+                    }
+                })
+                this.setState({posts: updatedPosts});
+                // console.log(updatedPosts)
+            });
+    }
+
     render () {
+
+        const posts = this.state.posts.map(
+            post => {
+                return <Post key={post.id} title={post.title} author={post.author}/>
+            }
+        );
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
