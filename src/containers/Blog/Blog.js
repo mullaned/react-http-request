5f3deee538bf287803ae9d3f592b444fb4c40068
David M <mullaned@gmail.com>
2019-06-06 08:49:37 +0100

    selectable post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios'
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -27,11 +28,20 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
 
         const posts = this.state.posts.map(
             post => {
-                return <Post key={post.id} title={post.title} author={post.author}/>
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}    
+                    />
             }
         );
 
@@ -41,7 +51,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
