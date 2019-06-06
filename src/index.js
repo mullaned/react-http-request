2c2e19ac012116395255aad6a36f03b93f9e4b6c
David M <mullaned@gmail.com>
2019-06-06 09:47:26 +0100

    axios default global config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios'
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+axios.defaults.headers.common['Authorization'] = 'Auth Token';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
   console.log(request);
   //Edit request Config
@@ -16,7 +21,7 @@ axios.interceptors.request.use(request => {
 
 axios.interceptors.response.use(response => {
   console.log(response);
-  //Edit request Config
+  //Edit response Config
   return response;
 }, error => {
   console.log(error);
