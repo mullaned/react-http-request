57becb72130ec394daf93097e96e3974f7f62f88
David M <mullaned@gmail.com>
2019-06-07 12:22:41 +0100

    re-organise for routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,59 +1,10 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-// import axios from 'axios'
-import axios from '../../axios'
+import Posts from '../Blog/Posts/Posts'
 
 class Blog extends Component {
-
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Dave'
-                    }
-                })
-                this.setState({posts: updatedPosts});
-                // console.log(updatedPosts)
-            })
-            .catch(error => {
-                console.log(error)
-                this.setState({error: true})
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
     render () {
-
-        let posts = <p style={{textAlign: "center"}}>Something went wrong!!</p>
-        if(!this.state.error){
-            posts = this.state.posts.map(
-            post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}    
-                    />
-            }
-        );
-        }
-         
 
         return (
             <div className="Blog">
@@ -66,16 +17,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
+                
                 <h1 style={{textAlign: "center"}}>React HTTP Requests with Axios</h1>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                
+                <Posts />
+                
             </div>
         );
     }
