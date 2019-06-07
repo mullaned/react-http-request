367f464ea0bb077a1b3a722a9fe3af376b27401c
David M <mullaned@gmail.com>
2019-06-07 13:12:31 +0100

    hoc withRouter

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -9,6 +9,10 @@ class NewPost extends Component {
         author: 'David'
     }
 
+    componentDidMount() {
+        console.log(this.props);
+    }
+
     postDataHandler = () => {
         const data = {
             title: this.state.title,
